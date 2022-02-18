a36e5c695b37853b32c90115a403d6b641a86ef0
Nathan Black <nblack@quanthub.com>
2022-02-18 14:30:38 -0600

    add workout generator

--- app.py
+++ app.py
@@ -1,4 +1,40 @@
 import streamlit as st
+import random
 
 st.title("🃏 Decksercise 🃏")
 st.subheader("A random workout generator")
+# Input number of cards
+#sample_size = st.sidebar.slider(label="Choose the number of cards to pull", min_value=1, max_value=54, value=25, step=1)
+rank =['Ace','King','Queen','Jack','2','3','4','5','6','7','8','9','10']
+suit =['Hearts ♥️','Clubs ♣️','Diamonds ♦️','Spades ♠️']
+suit_emojis = ["♥️", "♣️", "♦️", "♠️" ]
+deck = [x + " of " + y for x in rank for y in suit] + ["Joker", "Joker"]
+exercises = ['Push-ups', 'Sit-ups', 'Jumping jacks', 'Squats', "Sumo"]
+
+
+if "selected_exercises" not in st.session_state:
+    st.session_state.selected_exercises = random.sample(exercises, 4)
+
+if "selected_cards" not in st.session_state:
+    st.session_state.selected_cards = random.sample(deck, 54)
+
+if st.button("Generate new exercises"):
+    st.session_state.selected_exercises = random.sample(exercises, 4)
+
+for i in range(len(suit)):
+    st.write(f"{suit[i]} mean you're doing {st.session_state.selected_exercises[i]}")
+
+
+if st.button("Pick a card") and len(st.session_state.selected_cards) > 0:
+    selected_card = random.sample(st.session_state.selected_cards, 1)[0]
+    st.session_state.selected_cards.remove(selected_card)
+    st.write(selected_card)
+    if len(st.session_state.selected_cards) > 0:
+        st.write(f"{len(st.session_state.selected_cards)} to go!")
+
+if len(st.session_state.selected_cards) < 1:
+    st.write("Great job! You're done for today. See you tomorrow!!!")
+    st.balloons()
+
+
+
